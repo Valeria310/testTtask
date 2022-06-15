be3c89ad9e5e2c62808b64fe1363dec8a12da79b
Valeria Masaltseva <valeriua.m@yandex.ru>
2022-06-15 18:53:52 +0300

    fix: use eslint

--- test-task/src/app/note-board/components/note-board/note-board.component.spec.ts
+++ test-task/src/app/note-board/components/note-board/note-board.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoteBoardComponent } from './note-board.component';
 
 describe('NoteBoardComponent', () => {
-  let component: NoteBoardComponent;
-  let fixture: ComponentFixture<NoteBoardComponent>;
+    let component: NoteBoardComponent;
+    let fixture: ComponentFixture<NoteBoardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NoteBoardComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ NoteBoardComponent ]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(NoteBoardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(NoteBoardComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
